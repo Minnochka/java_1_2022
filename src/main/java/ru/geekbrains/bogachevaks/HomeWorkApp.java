3533a687824285cf62fb2d706b509399b498385d
minnochka <bogacheva.minna@yandex.ru>
2022-04-13 00:08:25 +0300

    add all tasks проверка языка

--- src/main/java/ru/geekbrains/bogachevaks/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/bogachevaks/HomeWorkApp.java
@@ -16,26 +16,26 @@ public class HomeWorkApp {
     }
 
     public static void checkSumSign(){
-        int a = 5, b = -10;
-        System.out.println("Введены числа " + a + " и " + b + ".");
+        int a = 5, b = -11;
+        System.out.println("Р’РІРµРґРµРЅС‹ С‡РёСЃР»Р° " + a + " Рё " + b + ".");
         if (a + b >= 0) {
-            System.out.println("Сумма положительная");
+            System.out.println("РЎСѓРјРјР° РїРѕР»РѕР¶РёС‚РµР»СЊРЅР°СЏ");
         }
         else{
-            System.out.println("Сумма отрицательная");
+            System.out.println("РЎСѓРјРјР° РѕС‚СЂРёС†Р°С‚РµР»СЊРЅР°СЏ");
         }
     }
 
     public static void  printColor(){
         int value = 114;
         if (value <= 0){
-            System.out.println("Красный");
+            System.out.println("РљСЂР°СЃРЅС‹Р№");
         }
         else if (value >= 100){
-            System.out.println("Зеленый");
+            System.out.println("Р—РµР»РµРЅС‹Р№");
         }
         else{
-            System.out.println("Желтый");
+            System.out.println("Р–РµР»С‚С‹Р№");
         }
     }
 
